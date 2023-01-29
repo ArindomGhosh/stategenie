cdcfec1cc3ea91750edb2d446f2a4711b3e21211
Arindom Ghosh <arindomghosh23@gmail.com>
2023-01-30 01:27:29 +0530

    created annotation processor

--- app/src/main/java/com/arindom/stategenie/presentation/DummyUiState.kt
+++ app/src/main/java/com/arindom/stategenie/presentation/DummyUiState.kt
@@ -1,6 +1,86 @@
 package com.arindom.stategenie.presentation
 
+import android.os.Parcelable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
 import com.arindom.stategenie.annotations.GenieState
+import com.arindom.stategenie.annotations.ToState
+import kotlinx.parcelize.Parcelize
 
-@GenieState
-interface DummyUiState
+
+interface BaseUIiState<T : Any> {
+    @ToState(
+        stateName = "Success"
+    )
+    val data: T
+
+    @ToState(
+        stateName = "Loading"
+    )
+    val loading: Unit
+
+    @ToState(
+        stateName = "Error"
+    )
+    val error: Throwable
+}
+
+@GenieState(
+//    rootName = "NewUiSate",
+    isParcelable = true
+)
+interface SomeUiState : BaseUIiState<DummyUiState> {
+//    @ToState(
+//        stateName = "LoggedOut"
+//    )
+    val loggedOut: Unit
+
+    @ToState(
+        stateName = "Wierd"
+    )
+    val someWierd: Boolean
+
+    @ToState(
+        stateName = "NonUi"
+    )
+    val nonUiStateValue: String
+
+}
+
+@GenieState(
+    rootName = "MovieUiState",
+)
+interface MovieStates : BaseUIiState<List<String>>
+
+@Immutable
+@Parcelize
+data class DummyUiState(
+    val data: List<String>
+) : Parcelable
+
+@Immutable
+data class PosterExtensive(
+    val posters: List<Poster>
+)
+
+@Immutable
+data class Poster(
+    val name: String,
+    val poster: String,
+    val playtime: String
+)
+
+fun List<Poster>.toExtensive(): PosterExtensive {
+    return PosterExtensive(this)
+}
+
+@Stable
+@Immutable
+data class PosterDetails(
+    val name: String,
+    val release: String,
+    val playtime: String,
+    val description: String,
+    val poster: String,
+    val gif: String
+)
--- genie-processors/src/main/java/com/arindom/stategenie/processors/GenieAnnotationVisitor.kt
+++ genie-processors/src/main/java/com/arindom/stategenie/processors/GenieAnnotationVisitor.kt
@@ -1,10 +1,17 @@
 package com.arindom.stategenie.processors
 
+import com.arindom.stategenie.annotations.GenieState
+import com.arindom.stategenie.annotations.ToState
+import com.arindom.stategenie.processors.util.getAnnotationIfExist
 import com.google.devtools.ksp.processing.CodeGenerator
 import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSPropertyDeclaration
+import com.google.devtools.ksp.symbol.KSType
 import com.google.devtools.ksp.symbol.KSVisitorVoid
-import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.*
+import com.squareup.kotlinpoet.ksp.toClassName
+import com.squareup.kotlinpoet.ksp.toTypeName
 import com.squareup.kotlinpoet.ksp.writeTo
 
 class GenieAnnotationVisitor(
@@ -15,10 +22,107 @@ class GenieAnnotationVisitor(
     override fun visitClassDeclaration(classDeclaration: KSClassDeclaration, data: Unit) {
         val packageName = classDeclaration.packageName.asString()
         val className = classDeclaration.simpleName.asString()
+        val subSetMap = mutableMapOf<String, Pair<String, KSType>>()
 
-        val fileName = "$className\$Generated"
-        val file = FileSpec.builder(packageName, fileName)
-            .build()
+        val arguments = classDeclaration.annotations.filter {
+            it.shortName.asString() == GenieState::class.simpleName
+        }.first().arguments
+        val rootName =
+            arguments.first { it.name?.asString() == GenieState.ROOT_NAME }.value as String
+        val isParcelable =
+            arguments.first { it.name?.asString() == GenieState.IS_PARCELABLE }.value as Boolean
+
+        val generatedClassName = if (rootName.isNotBlank()) {
+            if (rootName == className) "$rootName\$Generated"
+            else rootName
+        } else {
+            "$className\$Generated"
+        }
+
+        classDeclaration.getAllProperties().forEach {
+            println(it.simpleName.asString())
+            val subTypeDef = getSubTypDef(it)
+            println(subTypeDef)
+            if (subTypeDef != null) subSetMap[subTypeDef.first] = subTypeDef.second
+        }
+
+        val file = FileSpec.builder(packageName, generatedClassName).apply {
+            addFileComment(format = "This is generated file.")
+            addType(
+                TypeSpec.interfaceBuilder(generatedClassName)
+                    .addKdoc("An extensive sealed interface generated for $className.").apply {
+                        if (isParcelable) {
+                            addSuperinterface(ClassName("android.os", "Parcelable"))
+                        }
+                        addModifiers(KModifier.SEALED)
+                        subSetMap.forEach { (subTypeName, typeDef) ->
+                            addType(
+                                subType(
+                                    superType = ClassName(
+                                        packageName, generatedClassName
+                                    ),
+                                    subTypeName = subTypeName,
+                                    subTypeDef = typeDef,
+                                    isParcelable = isParcelable
+                                )
+                            )
+                        }
+                    }.build()
+            )
+        }.build()
         file.writeTo(codeGenerator, false)
+        subSetMap.clear()
+    }
+
+    private fun getSubTypDef(ksPropertyDeclaration: KSPropertyDeclaration): Pair<String, Pair<String, KSType>>? {
+        val toStateAnnotation = ToState::class.simpleName
+        val toState = ksPropertyDeclaration.annotations.getAnnotationIfExist(toStateAnnotation!!)
+        return if (toState != null) {
+            val suggestedName = toState.arguments.first {
+                it.name?.asString() == ToState.STATE_NAME
+            }.value as String
+            val stateName = suggestedName.ifBlank {
+                ksPropertyDeclaration.simpleName.asString()
+            }.replaceFirstChar {
+                if (it.isLowerCase()) it.titlecase() else it.toString()
+            }.plus("State")
+
+            Pair(
+                stateName, Pair(
+                    ksPropertyDeclaration.simpleName.asString(),
+                    ksPropertyDeclaration.type.resolve()
+                )
+            )
+        } else null
+    }
+
+    private fun subType(
+        superType: TypeName,
+        subTypeName: String,
+        subTypeDef: Pair<String, KSType>,
+        isParcelable: Boolean
+    ): TypeSpec {
+        val typeSpecBuilder = if (subTypeDef.second.toClassName().simpleName != "Unit") {
+            TypeSpec.classBuilder(subTypeName).apply {
+                val flux = FunSpec.constructorBuilder()
+                    .addParameter(subTypeDef.first, subTypeDef.second.toTypeName()).build()
+                addModifiers(KModifier.DATA)
+                primaryConstructor(flux)
+                addProperty(
+                    PropertySpec.builder(subTypeDef.first, subTypeDef.second.toTypeName())
+                        .addModifiers(KModifier.FINAL).initializer(subTypeDef.first).build()
+
+                )
+            }
+        } else {
+            TypeSpec.objectBuilder(subTypeName)
+        }
+
+        return typeSpecBuilder.apply {
+            if (isParcelable) {
+                addAnnotation(ClassName("kotlinx.parcelize", "Parcelize"))
+            }
+            addSuperinterface(superType)
+        }.build()
     }
 }
