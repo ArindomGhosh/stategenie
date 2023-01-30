8150c67866749ba76d380c9fbda563c05fffec45
Arindom Ghosh <arindomghosh23@gmail.com>
2023-01-30 17:45:55 +0530

    added unit test for processor (#1)
    * added unit test for precessor
    
    * added github workflow
    
    * added github workflow
    
    * added inheritedPath check for KotlinCompilation

---
+++ genie-processors/src/test/java/com/arindom/stategenie/processors/GenieStateProcessorTest.kt
@@ -0,0 +1,75 @@
+package com.arindom.stategenie.processors
+
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import com.tschuchort.compiletesting.kspWithCompilation
+import com.tschuchort.compiletesting.symbolProcessorProviders
+import org.junit.Assert
+import org.junit.Test
+
+class GenieStateProcessorTest {
+
+    @Test
+    fun `GeneState annotation should compile with success`() {
+        val sourceFile = """
+            package geneistate
+            
+            import com.arindom.stategenie.annotations.GenieState
+            import com.arindom.stategenie.annotations.ToState
+
+            interface BaseUIiState<T : Any> {
+                @ToState(
+                    stateName = "Success"
+                )
+                val data: T
+            
+                @ToState(
+                    stateName = "Loading"
+                )
+                val loading: Unit
+            
+                @ToState(
+                    stateName = "Error"
+                )
+                val error: Throwable
+            }
+
+            @GenieState(
+                rootName = "NewUiSate",
+                isParcelable = false
+            )
+            interface UiState : BaseUIiState<String> {
+                @ToState(
+                    stateName = "LoggedOut"
+                )
+                val loggedOut: Unit
+
+                @ToState(
+                    stateName = "Wierd"
+                )
+                val someWierd: Boolean
+
+                @ToState(
+                    stateName = "NonUi"
+                )
+                val nonUiStateValue: String
+
+            }
+        """.trimIndent()
+
+        val kotlinSource = SourceFile.kotlin(
+            name = "UiState.kt",
+            contents = sourceFile
+        )
+
+        val result = KotlinCompilation()
+            .apply {
+                sources = listOf(kotlinSource)
+                inheritClassPath = true
+                symbolProcessorProviders = listOf(GenieStateSymbolProcessorProvider())
+                verbose = false
+                kspWithCompilation = true
+            }.compile()
+        Assert.assertEquals(KotlinCompilation.ExitCode.OK, result.exitCode)
+    }
+}
