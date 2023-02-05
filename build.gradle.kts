529ee75603d952ca7e4117b66a0ef465c333f4e8
arindom-ghosh <arindomghosh23@gmail.com>
2023-02-06 01:38:07 +0530

    Feature/publih to artifact (#10)
    * feature: updated publish scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 buildscript {
+    repositories {
+        mavenCentral()
+    }
 
+    dependencies {
+        classpath("org.jfrog.buildinfo:build-info-extractor-gradle:4+")
+    }
 }
 
 plugins {
@@ -21,9 +27,19 @@ scmVersion {
     }
 }
 
-ext["artifactoryGroup"] = "com.arindom.stategenie"
-ext["versionName"] = scmVersion.version
-ext["versionCode"] = getVersionCode(scmVersion.version)
+ext.apply {
+    set("artifactoryGroup", "com.arindom.stategenie")
+    set("versionName", scmVersion.version)
+    set("versionCode", getVersionCode(scmVersion.version))
+    if (System.getenv("ARTIFACTORY_URL") != null)
+        set("artifactoryUrl", System.getenv("ARTIFACTORY_URL"))
+    if (System.getenv("USER_NAME") != null)
+        set("artifactoryUserName", System.getenv("USER_NAME"))
+    if (System.getenv("PASSWORD") != null)
+        set("artifactoryPassword", System.getenv("PASSWORD"))
+    if(System.getenv("REPOSITORY_KEY")!=null)
+        set("repositoryKey", System.getenv("REPOSITORY_KEY"))
+}
 
 fun getVersionCode(version: String): Int {
     val tokens = version.split(".", "-")
