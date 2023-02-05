e0bc049b4165fe607769a7416104158fa7132de8
Arindom Ghosh <arindomghosh23@gmail.com>
2023-02-05 15:40:06 +0530

    updated secret setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,9 +33,10 @@ ext.apply {
     set("versionCode", getVersionCode(scmVersion.version))
 //    set("versionName", "0.1.0-beta")
 //    set("versionCode", "0.1.0")
-    if (System.getenv("JF_ARTIFACTORY_URL") != null)
+    if (System.getenv("JF_ARTIFACTORY_URL") != null) {
         println(System.getenv("JF_ARTIFACTORY_URL"))
         set("artifactoryUrl", System.getenv("JF_ARTIFACTORY_URL"))
+    }
     if (System.getenv("JF_USER_NAME") != null)
         set("artifactoryUserName", System.getenv("JF_USER_NAME"))
     if (System.getenv("JF_PASSWORD") != null)
