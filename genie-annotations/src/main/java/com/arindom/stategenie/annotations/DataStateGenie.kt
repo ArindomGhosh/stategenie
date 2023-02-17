5455cbe6fdf66de5a532509bfc843657a4fbd2ee
Arindom Ghosh <arindomghosh23@gmail.com>
2023-02-17 18:56:56 +0530

    added DataStateBenie documentation

--- genie-annotations/src/main/java/com/arindom/stategenie/annotations/DataStateGenie.kt
+++ genie-annotations/src/main/java/com/arindom/stategenie/annotations/DataStateGenie.kt
@@ -17,6 +17,15 @@ package com.arindom.stategenie.annotations
 
 import kotlin.reflect.KClass
 
+/**
+ * [DataStateGenie] is applied on data class which contains all the properties which would impact your ui state.
+ * This would generate a single data class ${name}UiState and extention functions to update individual parameters,
+ * loading state and  error object of subtype of [Throwable] and default constructor function to get the default
+ * object as: ${name}UiState().
+ *
+ * @param dataStateConfig To provide the configuration for the generated UI state class
+ * @see [DataStateConfig]
+ * */
 @Retention(
   AnnotationRetention.SOURCE
 )
@@ -26,6 +35,18 @@ import kotlin.reflect.KClass
 annotation class DataStateGenie(
   val dataStateConfig: DataStateConfig = DataStateConfig()
 )
+
+/**
+ * [DataStateConfig] is used by [DataStateGenie] to provide configuration used by the SymbolProcessor
+ * to generate th ui state.
+ *
+ * @param name The optional name parameter is used for the name generated stata class as: ${name}UiState.
+ * In case not provided the name of the data class is used by default as: ${classname}UIState
+ * @param errorType The optional errorType is subtype of [Throwable] to make it customizable to be used
+ * to represent a set of allowed [Throwable] subtypes like sealed classes. In case not provided it defaults to
+ * [Throwable]
+ * @param isLoadingDefault The optional isLoadingDefault is set false by default.
+ * */
 
 @Retention(
   AnnotationRetention.SOURCE
